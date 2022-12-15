8c38d4d655d1d2451e321d2d0f35c90a5288fa0c
Axel Roffi <axel.roffi@gmail.com>
2022-12-15 16:53:19 +0100

    Create store_value_V1.py

---
+++ Examples/store_value/store_value_V1.py
@@ -0,0 +1,13 @@
+import smartpy as sp
+
+class StoreValue(sp.Contract):
+    def __init__(self):
+        self.init(
+            storedValue = 42
+        )
+
+@sp.add_test(name="Testing")
+def test():
+    scenario = sp.test_scenario()
+    contract = StoreValue()
+    scenario += contract
