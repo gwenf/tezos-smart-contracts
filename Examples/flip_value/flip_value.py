290604ec9bead02bc69c192f820ca1c03be85800
Axel Roffi <axel.roffi@gmail.com>
2022-12-15 16:44:28 +0100

    Add files via upload

--- Examples/flip_value/flip_value.py
+++ Examples/flip_value/flip_value.py
@@ -1,1 +1,11 @@
+import smartpy as sp
 
+class FlipValue(sp.Contract):
+    def __init__(self):
+        self.init(
+            bestDigit = 0
+        )
+    
+    @sp.entry_point
+    def flip(self):
+        self.data.bestDigit = 1 - self.data.bestDigit
