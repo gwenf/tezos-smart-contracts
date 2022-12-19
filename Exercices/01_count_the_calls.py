ecdab58cc7ac0367f0e7fafda74a86292d04fc9f
Axel Roffi <axel.roffi@gmail.com>
2022-12-19 12:50:40 +0100

    Create 01_count_the_calls.py

---
+++ Exercices/01_count_the_calls.py
@@ -0,0 +1,17 @@
+import smartpy as sp
+
+class CountTheCalls(sp.Contract):
+   def __init__(self):
+       self.init(nb_calls=0)
+
+   @sp.entry_point
+   def make_call(self):
+       self.data.nb_calls+=1
+
+@sp.add_test(name="Testing")
+def test():
+   scenario = sp.test_scenario()
+   contract = CountTheCalls()
+   scenario += contract
+   contract.make_call()
+   contract.make_call()
