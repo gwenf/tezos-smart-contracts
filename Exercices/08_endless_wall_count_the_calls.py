8123ab75e65328259dced265b7423908bed7f074
Axel Roffi <axel.roffi@gmail.com>
2022-12-20 18:33:25 +0100

    Update 08_endless_wall_count_the_calls.py

--- Exercices/08_endless_wall_count_the_calls.py
+++ Exercices/08_endless_wall_count_the_calls.py
@@ -1,11 +1,10 @@
 import smartpy as sp
 
 class EndlessWall(sp.Contract):
-   def __init__(self, initialText):
+    def __init__(self, initialText):
        self.init(wallText = initialText, nbCalls=0)
-
-   @sp.entry_point
-   def write_message(self, message):
+    @sp.entry_point
+    def write_message(self, message):
        sp.verify((sp.len(message)<= 30) & (sp.len(message)>=3), "message size invalid")
        self.data.wallText += ", " + message + " forever"
        self.data.nbCalls += 1
@@ -17,4 +16,11 @@ def test():
    scenario += c1
    scenario += c1.write_message("Ana & Jack")
    scenario += c1.write_message("freeCodeCamp")
-   scenario += c1.write_message("freeCodeCamp is a useful learning ressource")
+   scenario.verify(c1.data.wallText=="Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   scenario.verify(c1.data.nbCalls==2)
+   scenario += c1.write_message("this message is 31 letters long").run(valid=False)
+   scenario += c1.write_message("AB").run(valid=False)
+   #by default a transaction is valid, no need to add .run(valid=True) after testing the call
+   scenario += c1.write_message("LLL")
+   scenario += c1.write_message("this message is 30 characters ")
+   scenario.verify(c1.data.nbCalls==4)
