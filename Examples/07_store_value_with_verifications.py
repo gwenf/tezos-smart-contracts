eb37302aa1d50e8205fe79418e16f456be4ba315
Axel Roffi <axel.roffi@gmail.com>
2022-12-20 16:28:49 +0100

    Update 07_store_value_with_verifications.py

--- Examples/07_store_value_with_verifications.py
+++ Examples/07_store_value_with_verifications.py
@@ -6,11 +6,11 @@ class StoreValue(sp.Contract):
       
   @sp.entry_point
   def add(self,a):
-    sp.verify(a<10)
+    #sp.verify(a<10)
     #sp.verify(a<=9)
     #sp.verify(a<10,"Number less than 10")
     #sp.verify(a<10 & a>0), would fail because of priorities
-    #sp.verify((a<10) & (a>0), "Number strictly between 0 and 10")
+    sp.verify((a<10) & (a>0), "Number strictly between 0 and 10")
     self.data.storedValue += a
       
 @sp.add_test(name='Add')
@@ -18,5 +18,9 @@ def test():
    c1= StoreValue()
    scenario = sp.test_scenario()
    scenario += c1
-   c1.add(5) 
-    #show when replace with 15 or (-5) that would fail with both conditions
+   scenario.h1("testing add entrypoint")
+   c1.add(5)
+   c1.add(9)
+   scenario.verify(c1.data.storedValue==14)
+   c1.add(10).run(valid=False)
+   c1.add(0).run(valid=False)
