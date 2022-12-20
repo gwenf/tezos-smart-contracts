35a83a3836581faaba33af72e24d489789c16900
Axel Roffi <axel.roffi@gmail.com>
2022-12-20 18:47:49 +0100

    Update 08_store_value_with_multiple_parameters.py

--- Examples/08_store_value_with_multiple_parameters.py
+++ Examples/08_store_value_with_multiple_parameters.py
@@ -6,7 +6,7 @@ class StoreValue(sp.Contract):
         self.init(minValue=minValue, maxValue=maxValue)
 
     @sp.entry_point
-    def set(self, minValue, maxValue):
+    def set(self, newMinValue, newMaxValue):
             self.data.minValue = minValue
             self.data.maxValue = maxValue
     @sp.entry_point
@@ -15,13 +15,13 @@ class StoreValue(sp.Contract):
             self.data.maxValue += a
 @sp.add_test(name="testing")
 def test():
-        c1 = StoreValue(minValue =0, maxValue=5)
+        c1 = StoreValue(newMinValue =0, newMaxValue=5)
         scenario = sp.test_scenario()
         scenario+=c1
         c1.set(minValue=5, maxValue=10)
         #show that you have errors when not naming params in entrypoint call
         #c1.set(5, 10)
         c1.addNumber(20)
-        scenario.verify(c1.data.maxValue==30)
+        scenario.verify(c1.data.newMaxValue==30)
         c1.addNumber(-50)
-        scenario.verify(c1.data.minValue==-25)
+        scenario.verify(c1.data.newMinValue==-25)
