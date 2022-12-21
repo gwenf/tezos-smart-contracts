865b9c1e67d6b367541b259beacf6b82728bfb20
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 17:19:01 +0100

    Update 09_endless_wall_restrictions.py

--- Exercices/09_endless_wall_restrictions.py
+++ Exercices/09_endless_wall_restrictions.py
@@ -19,16 +19,16 @@ def test():
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
-   c1 = EndlessWall(initialText="Axel on Tezos forever", owner=alice)
+   c1 = EndlessWall(initialText = "Axel on Tezos forever", owner=alice)
    scenario = sp.test_scenario()
    scenario += c1
-   scenario += c1.write_message("Ana & Jack").run(sender=eve)
-   scenario += c1.write_message("freeCodeCamp").run(sender=bob)
+   scenario += c1.write_message("Ana & Jack").run(sender = eve)
+   scenario += c1.write_message("freeCodeCamp").run(sender = bob)
    scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
-   scenario += c1.write_message("freeCodeCamp").run(sender=bob, valid=False)
    scenario += c1.write_message("freeCodeCamp").run(sender = bob, valid = False)
-   scenario += c1.write_message("this message is 31 letters long").run(sender = alice, valid = False, )
-   #by default a transaction is valid, no need to add .run(valid = True) after testing a call that should be a valid transaction
+   scenario += c1.write_message("freeCodeCamp").run(sender = bob, valid = False)
+   scenario += c1.write_message("this message is 31 letters long").run(sender = alice, valid = False)
+   #by default a transaction is valid, no need to add .run(valid = True) after testing a valid call
    scenario += c1.write_message("LLL").run(sender = alice)
    scenario += c1.write_message("this message is 30 characters ").run(sender = eve)
    scenario.verify(c1.data.nbCalls == 4)
