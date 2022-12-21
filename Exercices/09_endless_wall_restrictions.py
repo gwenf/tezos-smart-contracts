e922dd2300a83680702523c55e135d0ca35ef437
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 17:17:43 +0100

    Update 09_endless_wall_restrictions.py

--- Exercices/09_endless_wall_restrictions.py
+++ Exercices/09_endless_wall_restrictions.py
@@ -4,11 +4,11 @@ import smartpy as sp
 
 class EndlessWall(sp.Contract):
    def __init__(self, initialText, owner):
-       self.init(wallText = initialText, nbCalls=0, lastSender = owner)
+       self.init(wallText = initialText, nbCalls = 0, lastSender = owner)
 
    @sp.entry_point
    def write_message(self, message):
-       sp.verify((sp.len(message)<= 30) & (sp.len(message)>=3), "invalid message size")
+       sp.verify((sp.len(message) <= 30) & (sp.len(message) >= 3), "invalid message size")
        sp.verify(self.data.lastSender != sp.sender, "Do not spam the wall" )
        self.data.wallText += ", " + message + " forever"
        self.data.nbCalls += 1
@@ -16,7 +16,6 @@ class EndlessWall(sp.Contract):
   
 @sp.add_test(name = "add my name")
 def test():
-   
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
@@ -25,4 +24,11 @@ def test():
    scenario += c1
    scenario += c1.write_message("Ana & Jack").run(sender=eve)
    scenario += c1.write_message("freeCodeCamp").run(sender=bob)
-   scenario += c1.write_message("freeCodeCamp").run(sender=bob)
+   scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   scenario += c1.write_message("freeCodeCamp").run(sender=bob, valid=False)
+   scenario += c1.write_message("freeCodeCamp").run(sender = bob, valid = False)
+   scenario += c1.write_message("this message is 31 letters long").run(sender = alice, valid = False, )
+   #by default a transaction is valid, no need to add .run(valid = True) after testing a call that should be a valid transaction
+   scenario += c1.write_message("LLL").run(sender = alice)
+   scenario += c1.write_message("this message is 30 characters ").run(sender = eve)
+   scenario.verify(c1.data.nbCalls == 4)
