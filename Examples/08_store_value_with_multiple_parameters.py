c4dcb23b6e518a9de4c8ac4b9dec21c811309a0f
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 16:28:34 +0100

    Update 08_store_value_with_multiple_parameters.py

--- Examples/08_store_value_with_multiple_parameters.py
+++ Examples/08_store_value_with_multiple_parameters.py
@@ -3,25 +3,29 @@ import smartpy as sp
 class StoreValue(sp.Contract):
 
     def __init__(self, minValue, maxValue):
-        self.init(minValue=minValue, maxValue=maxValue)
+        self.init(minValue = minValue, maxValue = maxValue)
 
     @sp.entry_point
     def set(self, newMinValue, newMaxValue):
-            self.data.minValue = minValue
-            self.data.maxValue = maxValue
+            self.data.minValue = newMinValue
+            self.data.maxValue = newMaxValue
+            
     @sp.entry_point
     def addNumber(self, a):
             self.data.minValue += a
             self.data.maxValue += a
-@sp.add_test(name="testing")
+            
+@sp.add_test(name = "testing")
 def test():
-        c1 = StoreValue(newMinValue =0, newMaxValue=5)
+        c1 = StoreValue(minValue = 0, maxValue = 5)
         scenario = sp.test_scenario()
-        scenario+=c1
-        c1.set(minValue=5, maxValue=10)
+        scenario += c1
+        c1.set(newMinValue = 5, newMaxValue = 10)
         #show that you have errors when not naming params in entrypoint call
         #c1.set(5, 10)
         c1.addNumber(20)
-        scenario.verify(c1.data.newMaxValue==30)
+        scenario.verify(c1.data.minValue == 25)
+        scenario.verify(c1.data.maxValue == 30)
         c1.addNumber(-50)
-        scenario.verify(c1.data.newMinValue==-25)
+        scenario.verify(c1.data.minValue == -25)
+        scenario.verify(c1.data.maxValue == -20)
