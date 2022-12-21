9c6d5a709dee2eb133e44c06faf174537df518d8
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 17:34:39 +0100

    Add files via upload

---
+++ Examples/09_store_value_only_owner_can_reset.py
@@ -0,0 +1,29 @@
+import smartpy as sp
+
+class StoreValue(sp.Contract):
+   def __init__(self, owner):
+       self.init(storedValue = sp.nat(42), owner = owner)
+
+   @sp.entry_point
+   def add(self, a):
+      sp.set_type(a, sp.TNat)
+      self.data.storedValue += a
+
+   @sp.entry_point
+   def reset(self):
+       sp.verify(sp.sender == self.data.owner, "only owner can reset")
+       self.data.storedValue = 0
+
+@sp.add_test(name="Testing")
+def test():
+   alice = sp.test_account("Alice").address
+   bob = sp.test_account("Bob").address
+   contract = StoreValue(owner = alice)
+   scenario = sp.test_scenario()
+   scenario += contract
+   contract.add(sp.nat(5))
+   scenario.verify(contract.data.storedValue == 47)
+   contract.reset().run(sender = bob)
+   contract.reset().run(sender = alice)
+   
+   
