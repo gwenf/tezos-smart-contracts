e202395e22cd843d748b396d960d1b6f671a8c42
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 16:30:27 +0100

    Update 01_count_the_calls.py

--- Exercices/01_count_the_calls.py
+++ Exercices/01_count_the_calls.py
@@ -2,13 +2,13 @@ import smartpy as sp
 
 class CountTheCalls(sp.Contract):
    def __init__(self):
-       self.init(nb_calls=0)
+       self.init(nb_calls = 0)
 
    @sp.entry_point
    def make_call(self):
-       self.data.nb_calls+=1
+       self.data.nb_calls += 1
 
-@sp.add_test(name="Testing")
+@sp.add_test(name = "Testing")
 def test():
    scenario = sp.test_scenario()
    contract = CountTheCalls()
