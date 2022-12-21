e9061abd27fd0e544b284c004cdcdfbfa2747fab
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 16:43:16 +0100

    Update 08_endless_wall_count_the_calls.py

--- Exercices/08_endless_wall_count_the_calls.py
+++ Exercices/08_endless_wall_count_the_calls.py
@@ -12,16 +12,16 @@ class EndlessWall(sp.Contract):
   
 @sp.add_test(name = "add my name")
 def test():
-   c1 = EndlessWall(initialText="Axel on Tezos forever")
+   c1 = EndlessWall(initialText = "Axel on Tezos forever")
    scenario = sp.test_scenario()
    scenario += c1
    scenario += c1.write_message("Ana & Jack")
    scenario += c1.write_message("freeCodeCamp")
    scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
    scenario.verify(c1.data.nbCalls == 2)
-   scenario += c1.write_message("this message is 31 letters long").run(valid=False)
-   scenario += c1.write_message("AB").run(valid=False)
-   #by default a transaction is valid, no need to add .run(valid=True) after testing a call that should be a valid transaction
+   scenario += c1.write_message("this message is 31 letters long").run(valid = False)
+   scenario += c1.write_message("AB").run(valid = False)
+   #by default a transaction is valid, no need to add .run(valid = True) after testing a call that should be a valid transaction
    scenario += c1.write_message("LLL")
    scenario += c1.write_message("this message is 30 characters ")
    scenario.verify(c1.data.nbCalls == 4)
