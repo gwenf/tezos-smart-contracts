f959ba30646832f1ac8caa9d80088e328a9c9345
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 16:05:35 +0100

    Update 08_endless_wall_count_the_calls.py

--- Exercices/08_endless_wall_count_the_calls.py
+++ Exercices/08_endless_wall_count_the_calls.py
@@ -2,10 +2,11 @@ import smartpy as sp
 
 class EndlessWall(sp.Contract):
     def __init__(self, initialText):
-       self.init(wallText = initialText, nbCalls=0)
+       self.init(wallText = initialText, nbCalls = 0)
+    
     @sp.entry_point
     def write_message(self, message):
-       sp.verify((sp.len(message)<= 30) & (sp.len(message)>=3), "message size invalid")
+       sp.verify((sp.len(message) <= 30) & (sp.len(message) >= 3), "message size invalid")
        self.data.wallText += ", " + message + " forever"
        self.data.nbCalls += 1
   
@@ -16,11 +17,11 @@ def test():
    scenario += c1
    scenario += c1.write_message("Ana & Jack")
    scenario += c1.write_message("freeCodeCamp")
-   scenario.verify(c1.data.wallText=="Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
-   scenario.verify(c1.data.nbCalls==2)
+   scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   scenario.verify(c1.data.nbCalls == 2)
    scenario += c1.write_message("this message is 31 letters long").run(valid=False)
    scenario += c1.write_message("AB").run(valid=False)
-   #by default a transaction is valid, no need to add .run(valid=True) after testing the call
+   #by default a transaction is valid, no need to add .run(valid=True) after testing a call that should be a valid transaction
    scenario += c1.write_message("LLL")
    scenario += c1.write_message("this message is 30 characters ")
-   scenario.verify(c1.data.nbCalls==4)
+   scenario.verify(c1.data.nbCalls == 4)
