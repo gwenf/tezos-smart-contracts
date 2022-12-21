38390100650dee9a078f89cdd7e481be9c268617
Axel Roffi <axel.roffi@gmail.com>
2022-12-21 16:39:22 +0100

    Update 05_store_value_multiply_entrypoint.py

--- Exercices/05_store_value_multiply_entrypoint.py
+++ Exercices/05_store_value_multiply_entrypoint.py
@@ -1,25 +1,27 @@
 import smartpy as sp
+
 class StoreValue(sp.Contract):
    def __init__(self):
-       self.init(stored_value=0)
+       self.init(storedValue = 0)
 
    @sp.entry_point
    def add(self,a):
-       self.data.stored_value += a
+       self.data.storedValue += a
       
    @sp.entry_point
    def reset(self):
-       self.data.stored_value = 0
+       self.data.storedValue = 0
 
    @sp.entry_point
    def multiply(self,factor):
-       self.data.stored_value *= factor
+       self.data.storedValue *= factor
 
-   @sp.add_test(name='Add')
+   @sp.add_test(name = "Testing")
    def test():
        c1= StoreValue()
        scenario = sp.test_scenario()
        scenario += c1
        c1.add(5)
+       scenario.verify(c1.data.storedValue == 5)
        c1.multiply(10)
-       scenario.verify(c1.data.stored_value==50)
+       scenario.verify(c1.data.storedValue == 50)
