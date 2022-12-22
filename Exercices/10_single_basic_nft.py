14694bdf27ad96681696ac5d77f9662795eb203a
Axel Roffi <axel.roffi@gmail.com>
2022-12-22 11:43:49 +0100

    Update 10_single_basic_nft.py

--- Exercices/10_single_basic_nft.py
+++ Exercices/10_single_basic_nft.py
@@ -2,16 +2,16 @@ import smartpy as sp
 
 class SingleBasicNFT(sp.Contract):
   def __init__(self, first_owner):
-      self.init(owner=first_owner, metadata = "Gwen's first NFT")
+      self.init(owner = first_owner, metadata = "Gwen's first NFT")
 
   @sp.entry_point
   def transfer(self, new_owner):
       sp.verify(sp.sender == self.data.owner, "not your property")
       self.data.owner = new_owner
 
-  @sp.add_test(name="update owner")
+  @sp.add_test(name = "update owner")
   def test():
-      alice= sp.test_account("alice").address
+      alice = sp.test_account("alice").address
       bob = sp.test_account("bob").address
       eve = sp.test_account("eve").address
       c1 = SingleBasicNFT(alice)
@@ -20,3 +20,4 @@ class SingleBasicNFT(sp.Contract):
       c1.transfer(bob).run(sender = alice)
       c1.transfer(eve).run(sender = bob)
       c1.transfer(alice).run(sender = eve)
+      scenario.verify(c1.data.owner == alice)
