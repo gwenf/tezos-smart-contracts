6b8a65d7adfe141a0ec74535023f604e5b3fe3ec
Axel Roffi <axel.roffi@gmail.com>
2023-01-02 12:22:19 +0100

    Update 06_store_value_help_type_inference.py

--- Examples/06_store_value_help_type_inference.py
+++ Examples/06_store_value_help_type_inference.py
@@ -2,12 +2,10 @@ import smartpy as sp
 
 class StoreValue(sp.Contract):
    def __init__(self):
-       #using sp.nat()
        self.init(storedValue = sp.nat(42))
 
    @sp.entry_point
    def add(self, a):
-       #helping with sp.set_type()
       sp.set_type(a, sp.TNat)
       self.data.storedValue += a
 
@@ -15,11 +13,10 @@ class StoreValue(sp.Contract):
    def reset(self):
        self.data.storedValue = 0
 
-@sp.add_test(name = "Testing")
+@sp.add_test(name="Testing")
 def test():
    scenario = sp.test_scenario()
    contract = StoreValue()
    scenario += contract
-    #helping with sp.nat() in scenario
+   scenario.h3("Helping with type inference")
    contract.add(sp.nat(5))
-    
