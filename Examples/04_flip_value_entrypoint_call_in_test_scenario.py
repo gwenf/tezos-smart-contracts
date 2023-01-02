7f590c77b4c3f0d8613583bde466e228cc40e22d
Axel Roffi <axel.roffi@gmail.com>
2023-01-02 12:14:42 +0100

    Update 04_flip_value_entrypoint_call_in_test_scenario.py

--- Examples/04_flip_value_entrypoint_call_in_test_scenario.py
+++ Examples/04_flip_value_entrypoint_call_in_test_scenario.py
@@ -15,6 +15,7 @@ def test():
     scenario = sp.test_scenario()
     contract = FlipValue()
     scenario += contract
+    scenario.h2('Testing flip entrypoint')
     contract.flip()
     contract.flip()
     contract.flip()
