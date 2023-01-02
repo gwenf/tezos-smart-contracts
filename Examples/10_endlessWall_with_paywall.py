56479e6a1437f2b4556a71c62dd3cd001f5d6c58
Axel Roffi <axel.roffi@gmail.com>
2023-01-02 12:42:24 +0100

    Update 10_endlessWall_with_paywall.py

--- Examples/10_endlessWall_with_paywall.py
+++ Examples/10_endlessWall_with_paywall.py
@@ -16,8 +16,6 @@ class EndlessWall(sp.Contract):
    def claim(self, requestedAmount):
         sp.verify(sp.sender == self.data.owner, "not your money")
         sp.send(self.data.owner, requestedAmount)
-       
-      
   
 @sp.add_test(name = "add my name")
 def test():
@@ -27,22 +25,22 @@ def test():
    c1 = EndlessWall(initialText = "Axel on Tezos forever", owner=alice)
    scenario = sp.test_scenario()
    scenario += c1
-    #scenario write_message ok
+   scenario.h3(" Testing write_message is ok ")
    scenario += c1.write_message("Ana & Jack").run(sender = eve, amount = sp.tez(1))
    scenario += c1.write_message("freeCodeCamp").run(sender = bob, amount = sp.tez(1))
    scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
-    #checking write_message fails for size message
+   scenario.h3(" Checking calls fail due to invalid size message ")
    scenario += c1.write_message("this message is 31 letters long").run(sender = alice, valid = False, amount = sp.tez(1))
    scenario += c1.write_message("AB").run(sender = alice, valid = False, amount = sp.tez(1))
-    #checking write_message passes for size message
+   scenario.h3(" Checking calls pass for limit size messages ")
    scenario += c1.write_message("LLL").run(sender = alice, amount = sp.tez(1))
    scenario += c1.write_message("this message has 30 characters").run(sender = eve, amount = sp.tez(1) )
    scenario.verify(c1.data.nbCalls == 4)
-   #checking testing amounts
+   scenario.h3(" Checking calls pass or fail for right amount")
    scenario += c1.write_message("testing right amount").run(sender = eve,amount = sp.tez(1))
    scenario += c1.write_message("testing lesser amount").run(sender = eve,amount = sp.mutez(999999), valid = False)
    scenario += c1.write_message("testing bigger amount").run(sender = bob, amount = sp.mutez(1000001), valid = False)
    scenario += c1.write_message("testing correct amount").run(sender = bob, amount = sp.tez(1))
-   #checking claim entrypoint
+   scenario.h3(" Checking only owner can claim balance in the contract")
    scenario += c1.claim(sp.tez(3)).run(sender = bob, valid = False)
    scenario += c1.claim(sp.tez(4)).run(sender = alice)
