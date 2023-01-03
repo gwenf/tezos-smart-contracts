7a3cf49877bf163507894133d675fe3f1a324100
Axel Roffi <axel.roffi@gmail.com>
2023-01-03 12:57:52 +0100

    Update 05_store_value_multiply_entrypoint.py

--- Exercices/05_store_value_multiply_entrypoint.py
+++ Exercices/05_store_value_multiply_entrypoint.py
@@ -21,7 +21,9 @@ class StoreValue(sp.Contract):
        c1= StoreValue()
        scenario = sp.test_scenario()
        scenario += c1
+       scenario.h3("Testing add entrypoint")
        c1.add(5)
        scenario.verify(c1.data.storedValue == 5)
+       scenario.h3("Testing multiply entrypoint")
        c1.multiply(10)
        scenario.verify(c1.data.storedValue == 50)
