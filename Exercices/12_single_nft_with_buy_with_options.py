b1d76e3cc41e801a77bd559ce586709a51db3f6e
Axel Roffi <axel.roffi@gmail.com>
2023-01-03 18:26:04 +0100

    Update 12_single_nft_with_buy_with_options.py

--- Exercices/12_single_nft_with_buy_with_options.py
+++ Exercices/12_single_nft_with_buy_with_options.py
@@ -11,7 +11,7 @@ class NftForSale(sp.Contract):
 
    @sp.entry_point
    def buy(self):
-       sp.verify(sp.amount == sp.open_some(self.data.price))
+       sp.verify(sp.some(sp.amount) == self.data.price)
        sp.send(self.data.owner, sp.amount)
        self.data.owner = sp.sender
        self.data.price = sp.none
@@ -20,9 +20,9 @@ class NftForSale(sp.Contract):
    def test():
        alice = sp.test_account('alice').address
        bob = sp.test_account('bob').address
-       c1 = NftForSale(owner=alice, metadata = "Gwen's NFT", price=sp.open_some(sp.mutez(5000000)))
+       c1 = NftForSale(owner=alice, metadata = "Gwen's NFT", price=sp.some(sp.mutez(5000000)))
        scenario = sp.test_scenario()
        scenario +=c1
-       c1.set_price(sp.some(sp.mutez(7000000)).run(sender=alice))
+       c1.set_price(sp.some(sp.mutez(7000000))).run(sender=alice)
        c1.buy().run(sender=bob, amount=sp.mutez(7000000))
        #scenario.verify(c1.data.owner == alice)
