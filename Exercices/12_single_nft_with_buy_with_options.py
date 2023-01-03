49b19920cb9e8b14d9b950685cb4e095547e7095
Axel Roffi <axel.roffi@gmail.com>
2023-01-03 18:18:45 +0100

    Update 12_single_nft_with_buy_with_options.py

--- Exercices/12_single_nft_with_buy_with_options.py
+++ Exercices/12_single_nft_with_buy_with_options.py
@@ -2,7 +2,7 @@ import smartpy as sp
 class NftForSale(sp.Contract):
 
    def __init__(self, owner, metadata, price):
-       self.init(owner = owner, metadata = "Gwen's NFT", price= price)
+       self.init(owner = owner, metadata = metadata, price= price)
        
    @sp.entry_point
    def set_price(self, new_price):
@@ -11,8 +11,8 @@ class NftForSale(sp.Contract):
 
    @sp.entry_point
    def buy(self):
-       sp.verify(sp.amount == self.data.price)
-       sp.send(self.data.owner, self.data.price)
+       sp.verify(sp.amount == sp.open_some(self.data.price))
+       sp.send(self.data.owner, sp.amount)
        self.data.owner = sp.sender
        self.data.price = sp.none
     
@@ -20,9 +20,9 @@ class NftForSale(sp.Contract):
    def test():
        alice = sp.test_account('alice').address
        bob = sp.test_account('bob').address
-       c1 = NftForSale(owner=alice, metadata='I am a string', price=sp.mutez(5000000))
+       c1 = NftForSale(owner=alice, metadata = "Gwen's NFT", price=sp.open_some(sp.mutez(5000000)))
        scenario = sp.test_scenario()
        scenario +=c1
-       c1.set_price(sp.mutez(7000000)).run(sender=alice)
+       c1.set_price(sp.some(sp.mutez(7000000)).run(sender=alice))
        c1.buy().run(sender=bob, amount=sp.mutez(7000000))
        #scenario.verify(c1.data.owner == alice)
