247f1ec5d8459866a2816a5148bf5a2c50a796a6
Axel Roffi <axel.roffi@gmail.com>
2023-01-31 18:55:11 +0100

    Update 15_nft_with_royalties.py

--- Exercices/15_nft_with_royalties.py
+++ Exercices/15_nft_with_royalties.py
@@ -1,24 +1,24 @@
 import smartpy as sp
 
 class NftForSale(sp.Contract):
-   def __init__(self, owner, metadata, price, author_fee, author):
-       self.init(owner = owner, metadata = metadata, price = price, author_fee = author_fee, author = author)
-
-   @sp.entry_point
-   def buy(self):
+    def __init__(self, owner, metadata, price, author_rate, author):
+           self.init(owner = owner, metadata = metadata, price = price, author_rate = sp.nat(5), author = author)
+           
+    @sp.entry_point
+    def buy(self):
        sp.verify(sp.amount == self.data.price)
-       author_fee = sp.split_tokens(self.data.price, 5, 100)
-       self.data.author_fee += author_fee
-       owner_fee = self.data.price - author_fee
-       sp.send(self.data.owner, owner_fee)
+       owner_share = sp.split_tokens(self.data.price, abs(100 - self.data.author_rate), 100)
+       sp.send(self.data.owner, owner_share)
        self.data.price+=sp.split_tokens(sp.amount, 10, 100)
        self.data.owner = sp.sender
+
+
        
 
-   @sp.entry_point
-   def claim_author_fee(self, requestedAmount):
+    @sp.entry_point
+    def claim_author_rate(self):
         sp.verify(sp.sender == self.data.author, " not your money ")
-        sp.send( self.data.author, requestedAmount)
+        sp.send( self.data.author, sp.balance)
 
 @sp.add_test(name='Testing')
 def test():
@@ -27,7 +27,7 @@ def test():
        eve = sp.test_account("eve").address
        author = sp.test_account("author").address
        c1 = NftForSale(owner = alice, metadata = "Gwen's nft", 
-       price=sp.mutez(5000000), author=author, author_fee = sp.mutez(1000000))
+       price=sp.mutez(5000000), author=author, author_rate = sp.mutez(1000000))
        scenario = sp.test_scenario()
        scenario +=c1
        scenario.h3(" Testing increasing price")
@@ -37,5 +37,5 @@ def test():
        
        scenario.verify(c1.data.price == sp.mutez(6050000))
        scenario.h3("Testing author fee claim")
-       c1.claim_author_fee(sp.tez(1)).run(sender = alice, valid = False)
-       c1.claim_author_fee(sp.mutez(525000)).run(sender = author)
+       c1.claim_author_rate().run(sender = alice, valid = False)
+       c1.claim_author_rate().run(sender = author)
