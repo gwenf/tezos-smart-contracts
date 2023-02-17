0a07aa8c196365b1368af9ae0d5b84e592c20658
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 22:10:00 +0100

    Update to new syntax

--- Exercices/15_nft_with_royalties.py
+++ Exercices/15_nft_with_royalties.py
@@ -1,21 +1,28 @@
 import smartpy as sp
 
-class NftForSale(sp.Contract):
-    def __init__(self, owner, metadata, price, author_rate, author):
-           self.init(owner = owner, metadata = metadata, price = price, author_rate = sp.nat(5), author = author)
-           
-    @sp.entry_point
-    def buy(self):
-       sp.verify(sp.amount == self.data.price)
-       owner_share = sp.split_tokens(self.data.price, abs(100 - self.data.author_rate), 100)
-       sp.send(self.data.owner, owner_share)
-       self.data.price+=sp.split_tokens(sp.amount, 10, 100)
-       self.data.owner = sp.sender
+@sp.module
+def main():
 
-    @sp.entry_point
-    def claim_author_rate(self):
-        sp.verify(sp.sender == self.data.author, " not your money ")
-        sp.send( self.data.author, sp.balance)
+    class NftForSale(sp.Contract):
+        def __init__(self, owner, metadata, price, author_rate, author):
+            self.data.owner = owner
+            self.data.metadata = metadata
+            self.data.price = price
+            self.data.author_rate = sp.nat(5)
+            self.data.author = author
+               
+        @sp.entrypoint
+        def buy(self):
+           assert sp.amount == self.data.price
+           owner_share = sp.split_tokens(self.data.price, sp.abs(100 - self.data.author_rate), 100)
+           sp.send(self.data.owner, owner_share)
+           self.data.price += sp.split_tokens(sp.amount, 10, 100)
+           self.data.owner = sp.sender
+    
+        @sp.entrypoint
+        def claim_author_rate(self):
+            assert sp.sender == self.data.author, " not your money "
+            sp.send(self.data.author, sp.balance)
 
 @sp.add_test(name='Testing')
 def test():
@@ -23,9 +30,9 @@ def test():
        bob = sp.test_account("bob").address
        eve = sp.test_account("eve").address
        author = sp.test_account("author").address
-       c1 = NftForSale(owner = alice, metadata = "Gwen's nft", 
+       c1 = main.NftForSale(owner = alice, metadata = "Gwen's nft", 
        price=sp.mutez(5000000), author=author, author_rate = sp.mutez(1000000))
-       scenario = sp.test_scenario()
+       scenario = sp.test_scenario(main)
        scenario +=c1
        scenario.verify(c1.data.price == sp.mutez(5000000) )
        scenario.h3(" Testing increasing price")
