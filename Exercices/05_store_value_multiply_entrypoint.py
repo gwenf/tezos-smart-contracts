7eca95ba1fdd65ec8893959d9ab40f1ecf2d998f
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:51:37 +0100

    Update to new syntax

--- Exercices/05_store_value_multiply_entrypoint.py
+++ Exercices/05_store_value_multiply_entrypoint.py
@@ -1,29 +1,32 @@
 import smartpy as sp
 
-class StoreValue(sp.Contract):
-   def __init__(self):
-       self.init(storedValue = 0)
+@sp.module
+def main():
 
-   @sp.entry_point
-   def add(self,a):
-       self.data.storedValue += a
-      
-   @sp.entry_point
-   def reset(self):
-       self.data.storedValue = 0
+    class StoreValue(sp.Contract):
+       def __init__(self):
+           self.data.storedValue = 0
+    
+       @sp.entrypoint
+       def add(self,a):
+           self.data.storedValue += a
+          
+       @sp.entrypoint
+       def reset(self):
+           self.data.storedValue = 0
+    
+       @sp.entrypoint
+       def multiply(self,factor):
+           self.data.storedValue *= factor
 
-   @sp.entry_point
-   def multiply(self,factor):
-       self.data.storedValue *= factor
-
-   @sp.add_test(name = "Testing")
-   def test():
-       c1= StoreValue()
-       scenario = sp.test_scenario()
-       scenario += c1
-       scenario.h3("Testing add entrypoint")
-       c1.add(5)
-       scenario.verify(c1.data.storedValue == 5)
-       scenario.h3("Testing multiply entrypoint")
-       c1.multiply(10)
-       scenario.verify(c1.data.storedValue == 50)
+@sp.add_test(name = "Testing")
+def test():
+   c1 = main.StoreValue()
+   scenario = sp.test_scenario(main)
+   scenario += c1
+   scenario.h3("Testing add entrypoint")
+   c1.add(5)
+   scenario.verify(c1.data.storedValue == 5)
+   scenario.h3("Testing multiply entrypoint")
+   c1.multiply(10)
+   scenario.verify(c1.data.storedValue == 50)
