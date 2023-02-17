fd4acd36eaf216e9c02fc160bace8e59eafe32af
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 20:56:05 +0100

    Update to new version

--- Examples/07_store_value_with_verifications.py
+++ Examples/07_store_value_with_verifications.py
@@ -1,22 +1,25 @@
 import smartpy as sp
 
-class StoreValue(sp.Contract):
-  def __init__(self):
-      self.init(storedValue = 0)
-      
-  @sp.entry_point
-  def add(self,a):
-    #sp.verify(a < 10)
-    #sp.verify(a <= 9)
-    #sp.verify(a < 10,"Number less than 10")
-    #sp.verify(a < 10 & a > 0), would fail because of priorities
-    sp.verify((a < 10) & (a > 0), "Number strictly between 0 and 10")
-    self.data.storedValue += a
+@sp.module
+def main():
+
+    class StoreValue(sp.Contract):
+      def __init__(self):
+          self.data.storedValue = 0
+          
+      @sp.entry_point
+      def add(self,a):
+        #assert a < 10
+        #assert a <= 9
+        #assert a < 10, "Number less than 10"
+        #assert a < 10 and a > 0, would fail because of priorities
+        assert (a < 10) and (a > 0), "Number strictly between 0 and 10"
+        self.data.storedValue += a
       
 @sp.add_test(name = 'Add')
 def test():
-   c1 = StoreValue()
-   scenario = sp.test_scenario()
+   c1 = main.StoreValue()
+   scenario = sp.test_scenario(main)
    scenario += c1
    scenario.h3("Testing add entrypoint")
    c1.add(1)
