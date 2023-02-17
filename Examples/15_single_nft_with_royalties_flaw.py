a4d6acd256647de3d195dc3b8a24215edbd18c12
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:30:18 +0100

    Update to new syntax

--- Examples/15_single_nft_with_royalties_flaw.py
+++ Examples/15_single_nft_with_royalties_flaw.py
@@ -1,18 +1,25 @@
 import smartpy as sp
 
-class SingleNftWithFlaw(sp.Contract):
-    def __init__(self,owner, metadata, price, author):
-        self.init(owner = owner, metadata = metadata, price= price, author = author, author_rate = sp.nat(5))
+@sp.module
+def main():
 
-    @sp.entry_point
-    def buy(self):
-       sp.verify(sp.amount == self.data.price)
-       author_share = sp.split_tokens(self.data.price, self.data.author_rate, 100)
-       owner_share = self.data.price - author_share
-       sp.send(self.data.owner, owner_share)
-       sp.send(self.data.author, author_share)
-       self.data.price += sp.split_tokens(self.data.price, 10, 100) 
-       self.data.owner = sp.sender
+    class SingleNftWithFlaw(sp.Contract):
+        def __init__(self,owner, metadata, price, author):
+            self.data.owner = owner
+            self.data.metadata = metadata
+            self.data.price = price
+            self.data.author = author
+            self.data.author_rate = sp.nat(5)
+    
+        @sp.entrypoint
+        def buy(self):
+           assert sp.amount == self.data.price
+           author_share = sp.split_tokens(self.data.price, self.data.author_rate, 100)
+           owner_share = self.data.price - author_share
+           sp.send(self.data.owner, owner_share)
+           sp.send(self.data.author, author_share)
+           self.data.price += sp.split_tokens(self.data.price, 10, 100) 
+           self.data.owner = sp.sender
 
 
 @sp.add_test(name='Testing')
@@ -21,8 +28,8 @@ def test():
        bob = sp.test_account("bob").address
        eve = sp.test_account("eve").address
        author = sp.test_account("author").address
-       c1 = SingleNftWithFlaw(owner = alice, metadata = "Gwen's first NFT", price = sp.mutez(5000000), author = author,)
-       scenario = sp.test_scenario()
+       c1 = main.SingleNftWithFlaw(owner = alice, metadata = "Gwen's first NFT", price = sp.mutez(5000000), author = author,)
+       scenario = sp.test_scenario(main)
        scenario +=c1    
        #testing buy entrypoint
        scenario.h3(" Testing buy entrypoint")
