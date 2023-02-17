050b919b7730d13ddf71680666548c1bbb0cd7b3
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:58:15 +0100

    Update to new syntax

--- Exercices/10_single_basic_nft.py
+++ Exercices/10_single_basic_nft.py
@@ -1,24 +1,28 @@
 import smartpy as sp
 
-class SingleBasicNFT(sp.Contract):
-  def __init__(self, first_owner):
-      self.init(owner = first_owner, metadata = "Gwen's first NFT")
+@sp.module
+def main():
 
-  @sp.entry_point
-  def transfer(self, new_owner):
-      sp.verify(sp.sender == self.data.owner, "not your property")
-      self.data.owner = new_owner
+    class SingleBasicNFT(sp.Contract):
+      def __init__(self, first_owner):
+          self.data.owner = first_owner
+          self.data.metadata = "Gwen's first NFT"
+    
+      @sp.entry_point
+      def transfer(self, new_owner):
+          assert sp.sender == self.data.owner, "not your property"
+          self.data.owner = new_owner
 
-  @sp.add_test(name = "update owner")
-  def test():
-      alice = sp.test_account("alice").address
-      bob = sp.test_account("bob").address
-      eve = sp.test_account("eve").address
-      c1 = SingleBasicNFT(alice)
-      scenario = sp.test_scenario()
-      scenario += c1
-      scenario.h3(" Testing transfer entrypoint")
-      c1.transfer(bob).run(sender = alice)
-      c1.transfer(eve).run(sender = bob)
-      c1.transfer(alice).run(sender = eve)
-      scenario.verify(c1.data.owner == alice)
+@sp.add_test(name = "update owner")
+def test():
+    alice = sp.test_account("alice").address
+    bob = sp.test_account("bob").address
+    eve = sp.test_account("eve").address
+    c1 = main.SingleBasicNFT(alice)
+    scenario = sp.test_scenario(main)
+    scenario += c1
+    scenario.h3(" Testing transfer entrypoint")
+    c1.transfer(bob).run(sender = alice)
+    c1.transfer(eve).run(sender = bob)
+    c1.transfer(alice).run(sender = eve)
+    scenario.verify(c1.data.owner == alice)
