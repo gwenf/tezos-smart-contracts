5cc392993aff9fde8fce601cc21f29ccd3587a27
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:41:38 +0100

    Update to new version (to fix)

--- Examples/15_yield_farming.py
+++ Examples/15_yield_farming.py
@@ -1,51 +1,53 @@
 import smartpy as sp
 
-class YieldFarming(sp.Contract):
-    def __init__(self, owner, lender, annual_yield_rate, ramp_up_duration):
-        self.init(owner = owner, 
-                  lender = lender, 
-                  deposit_amount = sp.tez(0), 
-                  deposit_date = sp.timestamp(0),
-                  annual_yield_rate = annual_yield_rate, 
-                  ramp_up_duration = ramp_up_duration)
+@sp.module
+def main():
 
-
-    @sp.entry_point
-    def owner_withdraw(self, requestedAmount):
-        sp.verify(sp.sender == self.data.owner)
-        one_year_yield = sp.split_tokens(self.data.deposit_amount, self.data.annual_yield_rate, 100)
-        reserve = self.data.deposit_amount + one_year_yield
-        sp.verify(sp.balance - requestedAmount >= reserve)
-        sp.send(sp.sender, requestedAmount)
-        
-    @sp.entry_point
-    def set_delegate(self, delegate_option):
-        sp.verify(sp.sender == self.data.owner)
-        sp.set_delegate(delegate_option)
-
-    @sp.entry_point
-    def deposit(self):
-        sp.verify(sp.sender == self.data.lender)
-        sp.verify(self.data.deposit_amount == sp.tez(0))
-        self.data.deposit_amount = sp.amount
-        self.data.deposit_date = sp.now
-        
-        
-    @sp.entry_point
-    def withdraw(self):
-        sp.verify(sp.sender == self.data.lender)
-        duration = sp.is_nat(sp.now - (self.data.deposit_date.add_seconds(self.data.ramp_up_duration))).open_some()
-        sp.trace(duration)
-        one_year= 365*24*3600
-        one_year_yield = sp.mul(self.data.deposit_amount, self.data.annual_yield_rate)
-        duration_yield = sp.split_tokens(one_year_yield, duration, one_year*100)
-        sp.send(sp.sender, self.data.deposit_amount + duration_yield )
-        self.data.deposit_amount = sp.tez(0)
-        self.data.deposit_date = sp.timestamp(0)
-
-    @sp.entry_point
-    def default(self):
-        pass
+    class YieldFarming(sp.Contract):
+        def __init__(self, owner, lender, annual_yield_rate, ramp_up_duration):
+            self.data.owner = owner
+            self.data.lender = lender
+            self.data.deposit_amount = sp.tez(0)
+            self.data.deposit_date = sp.timestamp(0)
+            self.data.annual_yield_rate = annual_yield_rate
+            self.data.ramp_up_duration = ramp_up_duration
+    
+    
+        @sp.entrypoint
+        def owner_withdraw(self, requestedAmount):
+            assert sp.sender == self.data.owner
+            one_year_yield = sp.split_tokens(self.data.deposit_amount, self.data.annual_yield_rate, 100)
+            reserve = self.data.deposit_amount + one_year_yield
+            assert sp.balance - requestedAmount >= reserve
+            sp.send(sp.sender, requestedAmount)
+            
+        @sp.entrypoint
+        def set_delegate(self, delegate_option):
+            assert sp.sender == self.data.owner
+            sp.set_delegate(delegate_option)
+    
+        @sp.entrypoint
+        def deposit(self):
+            assert sp.sender == self.data.lender
+            assert self.data.deposit_amount == sp.tez(0)
+            self.data.deposit_amount = sp.amount
+            self.data.deposit_date = sp.now
+            
+            
+        @sp.entrypoint
+        def withdraw(self):
+            assert sp.sender == self.data.lender
+            duration = sp.is_nat(sp.now - (self.data.deposit_date.add_seconds(self.data.ramp_up_duration))).unwrap_some()
+            one_year= 365*24*3600
+            one_year_yield = sp.mul(self.data.deposit_amount, self.data.annual_yield_rate)
+            duration_yield = sp.split_tokens(one_year_yield, duration, one_year*100)
+            sp.send(sp.sender, self.data.deposit_amount + duration_yield )
+            self.data.deposit_amount = sp.tez(0)
+            self.data.deposit_date = sp.timestamp(0)
+    
+        @sp.entrypoint
+        def default(self):
+            pass
 
 @sp.add_test(name = "Yield Farming")
 def test():
@@ -57,8 +59,8 @@ def test():
     voting_powers = {
         delegate.public_key_hash: 0,
     }
-    c1 = YieldFarming(owner = owner, lender = alice, annual_yield_rate = 4, ramp_up_duration = 3600*24*21)
-    scenario = sp.test_scenario()
+    c1 = main.YieldFarming(owner = owner, lender = alice, annual_yield_rate = 4, ramp_up_duration = 3600*24*21)
+    scenario = sp.test_scenario(main)
     scenario += c1
     scenario.h3("Testing default entrypoint")
     c1.default().run(sender=owner, amount= sp.tez(5))
