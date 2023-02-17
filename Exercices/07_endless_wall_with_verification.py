95934d8fd22a46bab922a87f9f98949306b191c5
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:54:06 +0100

    Update syntax

--- Exercices/07_endless_wall_with_verification.py
+++ Exercices/07_endless_wall_with_verification.py
@@ -1,18 +1,21 @@
 import smartpy as sp
 
-class EndlessWall(sp.Contract):
-   def __init__(self, initialText):
-       self.init(wallText = initialText)
+@sp.module
+def main():
 
-   @sp.entry_point
-   def write_message(self, message):
-       sp.verify((sp.len(message) <= 30) & (sp.len(message) >= 3), "invalid size message")
-       self.data.wallText += ", " + message + " forever"
+    class EndlessWall(sp.Contract):
+       def __init__(self, initialText):
+           self.data.wallText = initialText
+    
+       @sp.entry_point
+       def write_message(self, message):
+           assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid size message"
+           self.data.wallText += ", " + message + " forever"
   
 @sp.add_test(name = "add my name")
 def test():
-   c1 = EndlessWall(initialText = "Axel on Tezos forever")
-   scenario = sp.test_scenario()
+   c1 = main.EndlessWall(initialText = "Axel on Tezos forever")
+   scenario = sp.test_scenario(main)
    scenario += c1
    scenario.h3(" Testing write_message")
    scenario += c1.write_message("Ana & Jack")
