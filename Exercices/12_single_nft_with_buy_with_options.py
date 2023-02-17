f4bd7b9bc3818f4798e07c1a3070384ae80a3705
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 22:03:09 +0100

    Update to new Syntax

--- Exercices/12_single_nft_with_buy_with_options.py
+++ Exercices/12_single_nft_with_buy_with_options.py
@@ -1,28 +1,34 @@
 import smartpy as sp
-class NftForSale(sp.Contract):
 
-   def __init__(self, owner, metadata, price):
-       self.init(owner = owner, metadata = metadata, price= price)
-       
-   @sp.entry_point
-   def set_price(self, new_price):
-       sp.verify(sp.sender == self.data.owner, "you cannot update the price")
-       self.data.price = new_price
+@sp.module
+def main():
 
-   @sp.entry_point
-   def buy(self):
-       sp.verify(sp.some(sp.amount) == self.data.price)
-       sp.send(self.data.owner, sp.amount)
-       self.data.owner = sp.sender
-       self.data.price = sp.none
+    class NftForSale(sp.Contract):
     
-   @sp.add_test(name='Testing Update Price')
-   def test():
-       alice = sp.test_account('alice').address
-       bob = sp.test_account('bob').address
-       c1 = NftForSale(owner=alice, metadata = "Gwen's NFT", price=sp.some(sp.mutez(5000000)))
-       scenario = sp.test_scenario()
-       scenario +=c1
-       c1.set_price(sp.some(sp.mutez(7000000))).run(sender=alice)
-       c1.buy().run(sender=bob, amount=sp.mutez(7000000))
-       #scenario.verify(c1.data.owner == alice)
+       def __init__(self, owner, metadata, price):
+           self.data.owner = owner
+           self.data.metadata = metadata
+           self.data.price = price
+           
+       @sp.entrypoint
+       def set_price(self, new_price):
+           assert sp.sender == self.data.owner, "you cannot update the price"
+           self.data.price = new_price
+    
+       @sp.entrypoint
+       def buy(self):
+           assert sp.Some(sp.amount) == self.data.price
+           sp.send(self.data.owner, sp.amount)
+           self.data.owner = sp.sender
+           self.data.price = None
+    
+@sp.add_test(name='Testing Update Price')
+def test():
+    alice = sp.test_account('alice').address
+    bob = sp.test_account('bob').address
+    c1 = main.NftForSale(owner=alice, metadata = "Gwen's NFT", price=sp.some(sp.mutez(5000000)))
+    scenario = sp.test_scenario(main)
+    scenario +=c1
+    c1.set_price(sp.some(sp.mutez(7000000))).run(sender=alice)
+    c1.buy().run(sender=bob, amount=sp.mutez(7000000))
+    #scenario.verify(c1.data.owner == alice)
