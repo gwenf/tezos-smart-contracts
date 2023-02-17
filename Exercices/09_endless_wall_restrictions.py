31d390c21bc52d0f1e8699435da3aa663e57f2f6
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 21:57:01 +0100

    Update to new syntax (to fix)

--- Exercices/09_endless_wall_restrictions.py
+++ Exercices/09_endless_wall_restrictions.py
@@ -2,25 +2,30 @@
 #between two calls of a given user, at least one other user must have added text
 import smartpy as sp
 
-class EndlessWall(sp.Contract):
-   def __init__(self, initialText, owner):
-       self.init(wallText = initialText, nbCalls = 0, lastSender = owner)
+@sp.module
+def main():
 
-   @sp.entry_point
-   def write_message(self, message):
-       sp.verify((sp.len(message) <= 30) & (sp.len(message) >= 3), "invalid message size")
-       sp.verify(self.data.lastSender != sp.sender, "Do not spam the wall" )
-       self.data.wallText += ", " + message + " forever"
-       self.data.nbCalls += 1
-       self.data.lastSender = sp.sender
+    class EndlessWall(sp.Contract):
+       def __init__(self, initialText, owner):
+           self.data.wallText = initialText
+           self.data.nbCalls = 0
+           self.data.lastSender = owner
+    
+       @sp.entry_point
+       def write_message(self, message):
+           assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid message size"
+           assert self.data.lastSender != sp.sender, "Do not spam the wall"
+           self.data.wallText += ", " + message + " forever"
+           self.data.nbCalls += 1
+           self.data.lastSender = sp.sender
   
 @sp.add_test(name = "add my name")
 def test():
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
-   c1 = EndlessWall(initialText = "Axel on Tezos forever", owner=alice)
-   scenario = sp.test_scenario()
+   c1 = main.EndlessWall(initialText = "Axel on Tezos forever", owner=alice)
+   scenario = sp.test_scenario(main)
    scenario += c1
    scenario.h3("Testing write_message entrypoint ")
    scenario += c1.write_message("Ana & Jack").run(sender = eve)
