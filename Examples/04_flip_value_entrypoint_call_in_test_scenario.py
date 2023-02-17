aa2134b2d95c0e72acbd17bda868e9ad2e1d49a5
mathias-hiron <mathias.hiron@gmail.com>
2023-02-17 20:51:33 +0100

    Update to new version

--- Examples/04_flip_value_entrypoint_call_in_test_scenario.py
+++ Examples/04_flip_value_entrypoint_call_in_test_scenario.py
@@ -1,19 +1,20 @@
 import smartpy as sp
 
-class FlipValue(sp.Contract):
-    def __init__(self):
-        self.init(
-            bestDigit = 0
-        )
-    
-    @sp.entry_point
-    def flip(self):
-        self.data.bestDigit = 1 - self.data.bestDigit
+@sp.module
+def main():
+
+    class FlipValue(sp.Contract):
+        def __init__(self):
+            self.data.bestDigit = 0
+        
+        @sp.entrypoint
+        def flip(self):
+            self.data.bestDigit = 1 - self.data.bestDigit
 
 @sp.add_test(name = "Testing")
 def test():
-    scenario = sp.test_scenario()
-    contract = FlipValue()
+    scenario = sp.test_scenario(main)
+    contract = main.FlipValue()
     scenario += contract
     scenario.h2("Testing flip entrypoint")
     contract.flip()
