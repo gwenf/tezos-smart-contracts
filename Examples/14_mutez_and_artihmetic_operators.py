d0d4ceec807c7f3599a821d07ea401aa74d9dff8
Axel Roffi <axel.roffi@gmail.com>
2023-03-14 10:52:46 +0100

    Update 14_mutez_and_artihmetic_operators.py

--- Examples/14_mutez_and_artihmetic_operators.py
+++ Examples/14_mutez_and_artihmetic_operators.py
@@ -9,12 +9,12 @@ def main():
         
         @sp.entrypoint
         def add(self, x, y):
-            self.data.financial_value = nat_to_tez(x) + nat_to_tez(y)
+            self.data.financial_value = utils.nat_to_mutez(x) + utils.nat_to_mutez(y)
         
         
         @sp.entrypoint
         def sub(self, x, y):
-            self.data.financial_value = nat_to_tez(x) - nat_to_tez(y)
+            self.data.financial_value = utils.nat_to_mutez(x) - utils.nat_to_mutez(y)
             #WARNING sp.tez(<natural number>), sp.mutez(<natural number>): No negative value
        
         @sp.entrypoint
@@ -28,6 +28,17 @@ def main():
             #x being of type mutez and y of type nat
             #it returns and option of a pair and you can use open_some() to open the option
             # and then sp.fst to get the first element of the pair and sp.snd to get the rest
+
+
+@sp.add_test(name="testing operations")
+def test():
+    c1 = main.ComputingTez()
+    sc = sp.test_scenario(main)
+    sc += c1
+    c1.add(x = 5, y = 3)
+    c1.sub(x = 5, y = 3)
+    c1.multiply(x =  sp.tez(5), y = sp.nat(3))
+    c1.divide(x =  sp.tez(5), y = sp.nat(3))
 
 
 @sp.add_test(name="testing operations")
