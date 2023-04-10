47b104e6083c53c4a084220561c9da44ec5ee1b8
Axel Roffi <axel.roffi@gmail.com>
2023-04-10 17:57:55 +0200

    Update 17_true_endless_wall.py

--- Exercices/17_true_endless_wall.py
+++ Exercices/17_true_endless_wall.py
@@ -15,7 +15,14 @@ def main():
      
         @sp.entrypoint
         def write_message(self, text, timestamp):
-           self.data.messages[sp.sender] = sp.record(text = text, timestamp = timestamp)
+           data = sp.record(text = "", timestamp = sp.timestamp(0))
+           if self.data.messages.contains(sp.sender):
+               data = self.data.messages[sp.sender]
+               data.text += "," + text
+           else:
+               data.text = text
+           data.timestamp = timestamp
+           self.data.messages[sp.sender] = data
     
        
 @sp.add_test(name="testing truly endless wall")
@@ -27,4 +34,5 @@ def test():
     scenario = sp.test_scenario(main)
     c1 = main.TrulyEndlessWall(axel)
     scenario += c1
-    c1.write_message(text="bob's message", timestamp=sp.timestamp(20)).run(sender= bob)
+    c1.write_message(text="bob's message 1", timestamp=sp.timestamp(20)).run(sender= bob)
+    c1.write_message(text="bob's message 2", timestamp=sp.timestamp(30)).run(sender= bob)
