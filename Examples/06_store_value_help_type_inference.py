c597c44a3395fe57b365d4a8b6a3514d258bfbbf
Axel Roffi <axel.roffi@gmail.com>
2023-05-17 16:43:07 +0200

    Update 06_store_value_help_type_inference.py

--- Examples/06_store_value_help_type_inference.py
+++ Examples/06_store_value_help_type_inference.py
@@ -5,16 +5,16 @@ def main():
 
     class StoreValue(sp.Contract):
        def __init__(self):
-           self.data.storedValue = sp.nat(42)
+           self.data.stored_value = sp.nat(42)
     
        @sp.entrypoint
        def add(self, a):
           sp.cast(a, sp.nat)
-          self.data.storedValue += a
+          self.data.stored_value += a
     
        @sp.entrypoint
        def reset(self):
-           self.data.storedValue = 0
+           self.data.stored_value = 0
 
 @sp.add_test(name="Testing")
 def test():
