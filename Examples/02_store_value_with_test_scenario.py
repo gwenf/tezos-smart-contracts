57f46794974029c85bb217fdcab742def8a744fd
Axel Roffi <axel.roffi@gmail.com>
2023-05-17 16:38:29 +0200

    Update 02_store_value_with_test_scenario.py

--- Examples/02_store_value_with_test_scenario.py
+++ Examples/02_store_value_with_test_scenario.py
@@ -1,11 +1,10 @@
 import smartpy as sp
-
 @sp.module
 def main():
 
     class StoreValue(sp.Contract):
         def __init__(self):
-            self.data.storedValue = 42
+            self.data.stored_value = 42
 
 @sp.add_test(name="Testing")
 def test():
