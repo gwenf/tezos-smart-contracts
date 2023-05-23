9c7009d2def2694e518caf09031beb99fc1bf054
Axel Roffi <axel.roffi@gmail.com>
2023-05-23 16:29:24 +0200

    Create 09_using_trace.py

---
+++ Examples/09_using_trace.py
@@ -0,0 +1,23 @@
+import smartpy as sp
+
+@sp.module
+def main():
+
+    class CountTheCalls(sp.Contract):
+       def __init__(self):
+           self.data.nb_calls = 0
+    
+       @sp.entrypoint
+       def make_call(self):
+           self.data.nb_calls += 1
+           trace(self.data.nb_calls)
+           #Check browser console to get the execution of the code
+
+@sp.add_test(name="Testing")
+def test():
+   scenario = sp.test_scenario(main)
+   contract = main.CountTheCalls()
+   scenario += contract
+   scenario.h3(" Testing make_call entrypoint")
+   contract.make_call()
+   contract.make_call()
