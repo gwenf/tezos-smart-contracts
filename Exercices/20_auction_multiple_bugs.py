e9c6b982abf30ed1337f003e9a920b3ea44070d5
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 12:19:00 +0200

    from CamelCase to snake_case python style

--- Exercices/20_auction_multiple_bugs.py
+++ Exercices/20_auction_multiple_bugs.py
@@ -7,40 +7,40 @@ def main():
         def __init__(self):
             self.data.items = {}
             self.data.tokens = {}
-            self.data.tokenID = 1
+            self.data.token_id = 1
 
         @sp.entrypoint
         def mint(self, metadata):
-            self.data.tokens[self.data.tokenID] = sp.record(metadata = metadata, owner = sp.sender)
-            self.data.tokenID += 1
+            self.data.tokens[self.data.token_id] = sp.record(metadata = metadata, owner = sp.sender)
+            self.data.token_id += 1
         
         @sp.entrypoint
-        def openAuction(self, itemID, seller, deadline):
+        def open_auction(self, item_id, seller, deadline):
             # Bug 1, 2, 3, 4
             # bug 1
-            self.data.items[itemID] = sp.record(seller = seller,
+            self.data.items[item_id] = sp.record(seller = seller,
                                                 deadline = deadline,
-                                                topBid = sp.tez(0),
-                                                topBidder = seller
+                                                top_bid = sp.tez(0),
+                                                top_bidder = seller
                                                )
     
         @sp.entrypoint
-        def bid(self, itemID):
-            item = self.data.items[itemID]
-            assert sp.amount > item.topBid
+        def bid(self, item_id):
+            item = self.data.items[item_id]
+            assert sp.amount > item.top_bid
             assert sp.now < item.deadline
-            sp.send(item.topBidder, item.topBid) # Bug 4
-            item.topBid = sp.amount
-            item.topBidder = sp.sender
-            self.data.items[itemID] = item
+            sp.send(item.top_bidder, item.top_bid) # Bug 4
+            item.top_bid = sp.amount
+            item.top_bidder = sp.sender
+            self.data.items[item_id] = item
 
         @sp.entrypoint
-        def claimTopBid(self, itemID):
-            item = self.data.items[itemID]
+        def claim_top_bid(self, item_id):
+            item = self.data.items[item_id]
             assert sp.now >= item.deadline
             assert sp.sender == item.seller # Potential bug 6 if missing
-            sp.send(item.seller, item.topBid) # Bug 5
-            self.data.tokens[itemID].owner = item.topBidder # Bug 7
+            sp.send(item.seller, item.top_bid) # Bug 5
+            self.data.tokens[item_id].owner = item.top_bidder # Bug 7
 
 @sp.add_test(name='Testing extortion attack')
 def test():
@@ -52,7 +52,7 @@ def test():
     auctionContract = main.Auction()
     scenario += auctionContract
     auctionContract.mint("Mon NFT").run(sender = seller1)
-    auctionContract.openAuction(itemID = 1, seller = seller1, deadline = sp.timestamp(100))
+    auctionContract.open_auction(item_id = 1, seller = seller1, deadline = sp.timestamp(100))
     auctionContract.bid(1).run(sender = alice, amount = sp.tez(1), now = sp.timestamp(1))
     auctionContract.bid(1).run(sender = bob, amount = sp.tez(2), now = sp.timestamp(2))
-    auctionContract.claimTopBid(1).run(sender = seller1, now = sp.timestamp(101))
+    auctionContract.claim_top_bid(1).run(sender = seller1, now = sp.timestamp(101))
