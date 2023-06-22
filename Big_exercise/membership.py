81bcdc606701480074ee01dae4837e2a7cc8e1c3
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 16:51:56 +0200

    from CamelCase to snake_case python style

--- Big_exercise/membership.py
+++ Big_exercise/membership.py
@@ -4,12 +4,12 @@ import smartpy as sp
 def main():
   
 class Membership(sp.Contract):
-        def __init__(self, membership_price, owner, ledger, liquidityPool):
+        def __init__(self, membership_price, owner, ledger, liquidity_pool):
             self.data.owner = owner
             self.data.membership_price = membership_price
             self.data.members = sp.set()
             self.data.ledger = ledger
-            self.data.liquidityPool = liquidityPool
+            self.data.liquidity_pool = liquidity_pool
 
         @sp.entrypoint
         def join(self):
@@ -19,10 +19,10 @@ class Membership(sp.Contract):
             self.data.members.add(sp.sender)
 
         @sp.entrypoint
-        def join_with_tokens(self, nbTokens):
-            sp.cast(nbTokens, sp.nat)
-            tokenPrice =  sp.view("get_token_price", self.data.liquidityPool, (), sp.mutez).unwrap_some();
-            assert sp.mul(tokenPrice, nbTokens) > self.data.membership_price
+        def join_with_tokens(self, nb_tokens):
+            sp.cast(nb_tokens, sp.nat)
+            token_price =  sp.view("get_token_price", self.data.liquidity_pool, (), sp.mutez).unwrap_some();
+            assert sp.mul(token_price, nb_tokens) > self.data.membership_price
             ledger_contract = sp.contract(paramType, self.data.ledger, entrypoint="transfer").unwrap_some()
-            sp.transfer(sp.record(source = sp.sender, destination = self.data.owner, amount = nbTokens), sp.tez(0), ledger_contract)
+            sp.transfer(sp.record(source = sp.sender, destination = self.data.owner, amount = nb_tokens), sp.tez(0), ledger_contract)
             self.data.members.add(sp.sender)
