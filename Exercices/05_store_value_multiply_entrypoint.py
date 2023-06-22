0ee031237979be8a8b6923572ffc32ef22965563
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 11:40:56 +0200

    from CamelCase to snake_case python style

--- Exercices/05_store_value_multiply_entrypoint.py
+++ Exercices/05_store_value_multiply_entrypoint.py
@@ -5,19 +5,19 @@ def main():
 
     class StoreValue(sp.Contract):
        def __init__(self):
-           self.data.storedValue = 0
+           self.data.stored_value = 0
     
        @sp.entrypoint
        def add(self,a):
-           self.data.storedValue += a
+           self.data.stored_value += a
           
        @sp.entrypoint
        def reset(self):
-           self.data.storedValue = 0
+           self.data.stored_value = 0
     
        @sp.entrypoint
        def multiply(self,factor):
-           self.data.storedValue *= factor
+           self.data.stored_value *= factor
 
 @sp.add_test(name = "Testing")
 def test():
@@ -26,7 +26,7 @@ def test():
    scenario += c1
    scenario.h3("Testing add entrypoint")
    c1.add(5)
-   scenario.verify(c1.data.storedValue == 5)
+   scenario.verify(c1.data.stored_value == 5)
    scenario.h3("Testing multiply entrypoint")
    c1.multiply(10)
-   scenario.verify(c1.data.storedValue == 50)
+   scenario.verify(c1.data.stored_value == 50)
