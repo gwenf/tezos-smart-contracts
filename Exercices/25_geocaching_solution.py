1cd4ae0f93519149f9db6a3a8a65474fc7aa9b82
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 12:43:14 +0200

    from CamelCase to snake_case python style

--- Exercices/25_geocaching_solution.py
+++ Exercices/25_geocaching_solution.py
@@ -4,82 +4,82 @@ import smartpy as sp
 def main():
     class Geocaching(sp.Contract):
     
-        def __init__(self, owner, deadlineCommit, deadlineReveal, deadlineDispute, deposit):
+        def __init__(self, owner, deadline_commit, deadline_reveal, deadline_dispute, deposit):
             self.data.treasures = sp.big_map({})
-            self.data.scorePerUser = sp.big_map({})
+            self.data.score_per_user = sp.big_map({})
             self.data.commits = sp.big_map({})
-            self.data.currentWinner = owner
-            self.data.scorePerUser[owner] = 0
+            self.data.current_winner = owner
+            self.data.score_per_user[owner] = 0
             self.data.owner = owner
             self.data.deposit = deposit
-            self.data.deadlineCommit = deadlineCommit
-            self.data.deadlineReveal = deadlineReveal
-            self.data.deadlineDispute = deadlineDispute
-            self.data.nbTreasures = 0
+            self.data.deadline_commit = deadline_commit
+            self.data.deadline_reveal = deadline_reveal
+            self.data.deadline_dispute = deadline_dispute
+            self.data.nb_treasures = 0
 
         @sp.entrypoint
         def register_player(self):
-            assert sp.now < self.data.deadlineCommit
-            assert not self.data.scorePerUser.contains(sp.sender)
+            assert sp.now < self.data.deadline_commit
+            assert not self.data.score_per_user.contains(sp.sender)
             assert sp.amount == self.data.deposit
-            self.data.scorePerUser[sp.sender] = 0
+            self.data.score_per_user[sp.sender] = 0
         
         @sp.entrypoint
         def create_treasure(self, password_hash):
-            assert sp.now < self.data.deadlineCommit
+            assert sp.now < self.data.deadline_commit
             assert sp.sender == self.data.owner
-            self.data.nbTreasures += 1
-            self.data.treasures[self.data.nbTreasures] = sp.record(hash = password_hash, found = False, player = self.data.owner, password = sp.bytes("0x00"))
+            self.data.nb_treasures += 1
+            self.data.treasures[self.data.nb_treasures] = sp.record(hash = password_hash, found = False, player = self.data.owner, password = sp.bytes("0x00"))
 
         @sp.entrypoint
         def commit_discover_treasure(self, id, commit_data):
             assert sp.sender != self.data.owner
-            assert sp.now <= self.data.deadlineCommit
+            assert sp.now <= self.data.deadline_commit
             assert not self.data.commits.contains(commit_data)
             self.data.commits[commit_data] = True
         
         @sp.entrypoint
         def reveal_discover_treasure(self, id, password):
-            assert sp.now <= self.data.deadlineReveal
+            assert sp.now <= self.data.deadline_reveal
             treasure = self.data.treasures[id]
             assert not treasure.found
             self.data.treasures[id].found = True
             self.data.treasures[id].password = password
             self.data.treasures[id].player = sp.sender
-            self.data.scorePerUser[sp.sender] += 1
-            if self.data.scorePerUser[sp.sender] > self.data.scorePerUser[self.data.currentWinner]:
-                self.data.currentWinner = sp.sender
+            self.data.score_per_user[sp.sender] += 1
+            if self.data.score_per_user[sp.sender] > self.data.score_per_user[self.data.current_winner]:
+                self.data.current_winner = sp.sender
 
         # if someone detects that a reveal doesn't match a commitment, they can complain
         @sp.entrypoint
         def dispute_discovery(self, id):
-            assert sp.now <= self.data.deadlineDispute
+            assert sp.now <= self.data.deadline_dispute
             treasure = self.data.treasures[id]
             assert treasure.found
             if sp.blake2b(treasure.password) == treasure.hash:
                 expected_commit = sp.blake2b(sp.pack(sp.record(password = treasure.password, user = treasure.player)))
                 assert not self.data.commits.contains(expected_commit)
-            if self.data.scorePerUser.contains(treasure.player):
-                del self.data.scorePerUser[treasure.player]
+            if self.data.score_per_user.contains(treasure.player):
+                del self.data.score_per_user[treasure.player]
                 sp.send(sp.sender, self.data.deposit)
-            if treasure.player == self.data.currentWinner:
-                self.data.currentWinner = self.data.owner
+            if treasure.player == self.data.current_winner:
+                self.data.current_winner = self.data.owner
             self.data.treasures[id].found = False
             # we need to give more time for a player who really found it, to reveal it
-            self.data.deadlineReveal = sp.add_seconds(self.data.deadlineReveal, 3600)
-            self.data.deadlineDispute = sp.add_seconds(self.data.deadlineDispute, 3600)
+            self.data.deadline_reveal = sp.add_seconds(self.data.deadline_reveal, 3600)
+            self.data.deadline_dispute = sp.add_seconds(self.data.deadline_dispute, 3600)
 
         # If we eliminated the winner, we can't do a loop to recompute it, so we let the real winner claim their place
         @sp.entrypoint
         def update_winner(self):
-            assert sp.now <= self.data.deadlineDispute
-            assert self.data.scorePerUser[sp.sender] > self.data.scorePerUser[self.data.currentWinner]
-            self.data.currentWinner = sp.sender
+            assert sp.now <= self.data.deadline_dispute
+            assert self.data.score_per_user[sp.sender] > self.data.score_per_user[self.data.current_winner]
+            self.data.current_winner = sp.sender
 
         @sp.entrypoint
         def claim_prize(self):
-            assert sp.now > self.data.deadlineDispute
-            assert sp.sender == self.data.currentWinner
+            assert sp.now > self.data.deadline_dispute
+            assert sp.sender == self.data.current_winner
             sp.send(sp.sender, sp.balance)
                    
 @sp.add_test(name='Geocaching test')
@@ -125,11 +125,3 @@ def test():
     geocaching.claim_prize().run(sender = carl, now = sp.timestamp(1000), valid = False)
     geocaching.claim_prize().run(sender = alice, now = sp.timestamp(1000), valid=False)
     
-
-
-        
-
-    
-
-
-        
