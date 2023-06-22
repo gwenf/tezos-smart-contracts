bc129dde9de98f09c827adc259117166f3a9ae73
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 12:22:08 +0200

    from CamelCase to snake_case python style

--- Exercices/20_auction_multiple_bugz_solution.py
+++ Exercices/20_auction_multiple_bugz_solution.py
@@ -7,7 +7,7 @@ def main():
         def __init__(self, admin):
             self.data.items = sp.big_map({})
             self.data.tokens = sp.big_map({})
-            self.data.tokenID = 1
+            self.data.token_id = 1
             self.data.admin = admin
             self.data.tez_to_claim = sp.big_map({})
 
@@ -25,39 +25,39 @@ def main():
             if not self.data.tez_to_claim.contains(self.data.admin):
                 self.data.tez_to_claim[self.data.admin] = sp.tez(0)
             self.data.tez_to_claim[self.data.admin] += sp.amount
-            self.data.tokens[self.data.tokenID] = sp.record(metadata = metadata, owner = sp.sender)
-            self.data.tokenID += 1
+            self.data.tokens[self.data.token_id] = sp.record(metadata = metadata, owner = sp.sender)
+            self.data.token_id += 1
         
         @sp.entrypoint
-        def openAuction(self, itemID, deadline):
-            assert sp.sender == self.data.tokens[itemID].owner
-            assert not self.data.items.contains(itemID)
-            self.data.items[itemID] = sp.record( deadline = deadline,
-                                                topBid = sp.tez(0),
-                                                topBidder = sp.sender
+        def open_auction(self, item_id, deadline):
+            assert sp.sender == self.data.tokens[item_id].owner
+            assert not self.data.items.contains(item_id)
+            self.data.items[item_id] = sp.record( deadline = deadline,
+                                                top_bid = sp.tez(0),
+                                                top_bidder = sp.sender
                                                )
     
         @sp.entrypoint
-        def bid(self, itemID):
-            item = self.data.items[itemID]
-            assert sp.amount > item.topBid
+        def bid(self, item_id):
+            item = self.data.items[item_id]
+            assert sp.amount > item.top_bid
             assert sp.now < item.deadline
-            if not self.data.tez_to_claim.contains(item.topBidder):
-                self.data.tez_to_claim[item.topBidder] = sp.tez(0)
-            self.data.tez_to_claim[item.topBidder] += item.topBid
-            item.topBid = sp.amount
-            item.topBidder = sp.sender
-            self.data.items[itemID] = item
+            if not self.data.tez_to_claim.contains(item.top_bidder):
+                self.data.tez_to_claim[item.top_bidder] = sp.tez(0)
+            self.data.tez_to_claim[item.top_bidder] += item.top_bid
+            item.top_bid = sp.amount
+            item.top_bidder = sp.sender
+            self.data.items[item_id] = item
 
         @sp.entrypoint
-        def claimTopBid(self, itemID):
-            item = self.data.items[itemID]
-            token = self.data.tokens[itemID]
+        def claim_top_bid(self, item_id):
+            item = self.data.items[item_id]
+            token = self.data.tokens[item_id]
             assert sp.now >= item.deadline
-            assert sp.sender == token.owner or sp.sender == item.topBidder
-            del self.data.items[itemID]
-            sp.send(token.owner, item.topBid)
-            self.data.tokens[itemID].owner = item.topBidder
+            assert sp.sender == token.owner or sp.sender == item.top_bidder
+            del self.data.items[item_id]
+            sp.send(token.owner, item.top_bid)
+            self.data.tokens[item_id].owner = item.top_bidder
 
 @sp.add_test(name='Testing extortion attack')
 def test():
@@ -66,10 +66,10 @@ def test():
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
     scenario = sp.test_scenario(main)
-    auctionContract = main.Auction(alice)
-    scenario += auctionContract
-    auctionContract.mint("Mon NFT").run(sender = seller1, amount = sp.tez(1))
-    auctionContract.openAuction(itemID = 1, deadline = sp.timestamp(100)).run(sender=seller1)
-    auctionContract.bid(1).run(sender = alice, amount = sp.tez(1), now = sp.timestamp(1))
-    auctionContract.bid(1).run(sender = bob, amount = sp.tez(2), now = sp.timestamp(2))
-    auctionContract.claimTopBid(1).run(sender = seller1, now = sp.timestamp(101))
+    auction_contract = main.Auction(alice)
+    scenario += auction_contract
+    auction_contract.mint("Mon NFT").run(sender = seller1, amount = sp.tez(1))
+    auction_contract.open_auction(item_id = 1, deadline = sp.timestamp(100)).run(sender=seller1)
+    auction_contract.bid(1).run(sender = alice, amount = sp.tez(1), now = sp.timestamp(1))
+    auction_contract.bid(1).run(sender = bob, amount = sp.tez(2), now = sp.timestamp(2))
+    auction_contract.claim_top_bid(1).run(sender = seller1, now = sp.timestamp(101))
