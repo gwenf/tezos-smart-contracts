534ea739510a97d929d03c1b27df4ac8b7e64fed
Axel Roffi <axel.roffi@gmail.com>
2023-06-22 10:51:17 +0200

    CamelCase to snake_case python style

--- Examples/11_endlesswall_with_options.py
+++ Examples/11_endlesswall_with_options.py
@@ -4,30 +4,30 @@ import smartpy as sp
 def main():
 
     class EndlessWall(sp.Contract):
-       def __init__(self, initialText, owner):
-           self.data.wallText = initialText
+       def __init__(self, initial_text, owner):
+           self.data.wall_text = initial_text
            self.data.nbCalls = 0
-           self.data.lastSender = None
+           self.data.last_sender = None
     
        @sp.entrypoint
        def write_message(self, message):
            assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid message size"
-           assert self.data.lastSender != sp.Some(sp.sender), "Do not spam the wall"
-           self.data.wallText += ", " + message + " forever"
+           assert self.data.last_sender != sp.Some(sp.sender), "Do not spam the wall"
+           self.data.wall_text += ", " + message + " forever"
            self.data.nbCalls += 1
-           self.data.lastSender = sp.Some(sp.sender)  
+           self.data.last_sender = sp.Some(sp.sender)  
   
 @sp.add_test(name = "add my name")
 def test():
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
-   c1 = main.EndlessWall(initialText = "Axel on Tezos forever", owner = alice)
+   c1 = main.EndlessWall(initial_text = "Axel on Tezos forever", owner = alice)
    scenario = sp.test_scenario(main)
    scenario += c1
    c1.write_message("Ana & Jack").run(sender = eve)
    c1.write_message("freeCodeCamp").run(sender = bob)
-   scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   scenario.verify(c1.data.wall_text == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
    c1.write_message("freeCodeCamp").run(sender = bob, valid = False, exception="Do not spam the wall")
    c1.write_message("this message is 31 letters long").run(sender = alice, valid = False)
    #by default a transaction is valid, no need to add .run(valid = True) after testing a valid call
