b4d7dd041d3c23fc5ccfc951f4cccacbb0d73646
Axel Roffi <axel.roffi@gmail.com>
2023-06-28 15:56:17 +0200

    change address specification in tests

--- Examples/09_store_value_only_owner_can_reset.py
+++ Examples/09_store_value_only_owner_can_reset.py
@@ -3,30 +3,37 @@ import smartpy as sp
 @sp.module
 def main():
 
-    class StoreValue(sp.Contract):
-       def __init__(self, owner):
-           self.data.store_value = 42
-           self.data.owner = owner
+    class EndlessWall(sp.Contract):
+       def __init__(self, initial_text, owner):
+           self.data.wall_text = initial_text
+           self.data.nbCalls = 0
+           self.data.last_sender = None
     
        @sp.entrypoint
-       def add(self, a):
-          self.data.store_value += a
-    
-       @sp.entrypoint
-       def reset(self):
-           assert sp.sender == self.data.owner, "only owner can reset"
-           self.data.store_value = 0
-
-@sp.add_test(name="Testing")
+       def write_message(self, message):
+           assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid message size"
+           assert self.data.last_sender != sp.Some(sp.sender), "Do not spam the wall"
+           self.data.wall_text += ", " + message + " forever"
+           self.data.nbCalls += 1
+           self.data.last_sender = sp.Some(sp.sender)  
+  
+@sp.add_test(name = "add my name")
 def test():
-   alice = sp.test_account("Alice").address
-   bob = sp.test_account("Bob").address
-   contract = main.StoreValue(owner = alice)
+   alice=sp.test_account("Alice")
+   bob=sp.test_account("Bob")
+   eve=sp.test_account("Eve")
+   c1 = main.EndlessWall(initial_text = "Axel on Tezos forever", owner = alice.address)
    scenario = sp.test_scenario(main)
-   scenario += contract
-   scenario.h3("Testing add entrypoint")
-   contract.add(5)
-   scenario.verify(contract.data.store_value == 47)
+   scenario += c1
+   c1.write_message("Ana & Jack").run(sender = eve)
+   c1.write_message("freeCodeCamp").run(sender = bob)
+   scenario.verify(c1.data.wall_text == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   c1.write_message("freeCodeCamp").run(sender = bob, valid = False, exception="Do not spam the wall")
+   c1.write_message("this message is 31 letters long").run(sender = alice, valid = False)
+   #by default a transaction is valid, no need to add .run(valid = True) after testing a valid call
+   c1.write_message("LLL").run(sender = alice)
+   c1.write_message("this message is 30 characters ").run(sender = eve)
+   scenario.verify(c1.data.nbCalls == 4)
    scenario.h3(" Testing reset entrypoint, only owner can reset")
    contract.reset().run(sender = bob, valid = False)
    contract.reset().run(sender = alice)
